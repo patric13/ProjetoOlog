93eda887774ab105c8ee16c13fccde54c990f6ba
Patric <patricoliveira1@gmail.com>
2016-04-27 22:15:19 -0300

    Commit do projeto Mobile

---
+++ Projeto/ProjetoOlog/ProjetoOlog/Controller/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoOlog.Controller
+{
+   public  class LoginController
+    {
+
+        public void RealizarLogin(string usuario, string senha) {
+
+        }
+    }
+}
---
+++ Projeto/ProjetoOlog/ProjetoOlog/View/TransmissaoView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace ProjetoOlog
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TransmissaoView : Page
+    {
+        bool Transmitindo;
+        public TransmissaoView()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void btnTransmissaoClick(object sender, RoutedEventArgs e)
+        {
+            if (this.Transmitindo)
+            {
+                PararTransmissao();
+                btnTransmissao.Content = "Inciar Transmissão";
+            }
+            else
+            {
+                IniciarTransmissao();
+                btnTransmissao.Content = "Parar Transmissão";
+            }
+        }
+
+        private void IniciarTransmissao() {
+            //controlador
+            this.Transmitindo = true;
+        }
+
+        private void PararTransmissao() {
+            //controlador
+            this.Transmitindo = false;
+        }
+    }
+}
